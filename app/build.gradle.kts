f028279fa86367b2bbbf55a5ff76c3fb3e8c6569
ksw4015 <zealot4321@naver.com>
2024-09-25 13:13:07 +0900

    test: add AreaCodeRepository test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,4 +95,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.compiler)
 }
