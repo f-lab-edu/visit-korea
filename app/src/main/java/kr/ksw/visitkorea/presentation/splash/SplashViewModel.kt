98dea22d3f55b206efaea2b4a9d9d200dd941ac9
ksw4015 <zealot4321@naver.com>
2024-09-26 20:00:52 +0900

    feat: add AreaCodeWorker with hilt worker

--- app/src/main/java/kr/ksw/visitkorea/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/kr/ksw/visitkorea/presentation/splash/SplashViewModel.kt
@@ -1,6 +1,7 @@
 package kr.ksw.visitkorea.presentation.splash
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -8,15 +9,22 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import kr.ksw.visitkorea.data.local.databases.AreaCodeDatabase
+import kr.ksw.visitkorea.data.workmanager.AreaCodeWorker
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val areaCodeDatabase: AreaCodeDatabase
+): ViewModel() {
     private val _sideEffect = MutableSharedFlow<SplashSideEffect>(replay = 0)
     val sideEffect: SharedFlow<SplashSideEffect> = _sideEffect.asSharedFlow()
 
@@ -43,6 +51,23 @@ class SplashViewModel @Inject constructor(): ViewModel() {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ))
+        }
+    }
+
+    fun initAreaCode(context: Context) {
+        viewModelScope.launch {
+            val areaCodeItems = areaCodeDatabase.areaCodeDao.getAllAreaCodeEntities()
+            if(areaCodeItems.isEmpty()) {
+                val workRequest = OneTimeWorkRequestBuilder<AreaCodeWorker>().build()
+                WorkManager
+                    .getInstance(context)
+                    .enqueue(workRequest)
+            } else {
+                Log.d("SplashViewModel", areaCodeItems.toString())
+                areaCodeItems.forEach {
+                    Log.d("SplashViewModel", areaCodeDatabase.areaCodeDao.getSigunguCodeByAreaCode(it.code).toString())
+                }
+            }
         }
     }
 }
