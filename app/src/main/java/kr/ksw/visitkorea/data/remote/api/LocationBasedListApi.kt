ea88e9f834e6f837d59c250bf98d32b9addc6d11
ksw4015 <zealot4321@naver.com>
2024-09-28 16:46:30 +0900

    feat: add LocationBasedListApi, LocationBasedListRepository

---
+++ app/src/main/java/kr/ksw/visitkorea/data/remote/api/LocationBasedListApi.kt
@@ -0,0 +1,18 @@
+package kr.ksw.visitkorea.data.remote.api
+
+import kr.ksw.visitkorea.data.remote.dto.LocationBasedDTO
+import kr.ksw.visitkorea.data.remote.model.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LocationBasedListApi {
+    @GET("areaCode1")
+    suspend fun getLocationBasedListByContentType(
+        @Query("radius") radius: String = "5000",
+        @Query("numOfRows") numOfRows: Int,
+        @Query("pageNo") pageNo: Int,
+        @Query("mapX") mapX: String,
+        @Query("mapY") mapY: String,
+        @Query("contentTypeId") contentTypeId: String
+    ): ApiResponse<LocationBasedDTO>
+}
---
+++ app/src/main/java/kr/ksw/visitkorea/data/repository/LocationBasedListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package kr.ksw.visitkorea.data.repository
+
+import kr.ksw.visitkorea.data.mapper.toItems
+import kr.ksw.visitkorea.data.remote.api.LocationBasedListApi
+import kr.ksw.visitkorea.data.remote.dto.LocationBasedDTO
+import javax.inject.Inject
+
+class LocationBasedListRepositoryImpl @Inject constructor(
+    private val locationBasedListApi: LocationBasedListApi
+): LocationBasedListRepository {
+    override suspend fun getLocationBasedListByContentType(
+        numOfRows: Int,
+        pageNo: Int,
+        mapX: String,
+        mapY: String,
+        contentTypeId: String
+    ): Result<List<LocationBasedDTO>> = runCatching {
+        locationBasedListApi.getLocationBasedListByContentType(
+            numOfRows = numOfRows,
+            pageNo = pageNo,
+            mapX = mapY,
+            mapY = mapY,
+            contentTypeId = contentTypeId
+        ).toItems()
+    }
+}
