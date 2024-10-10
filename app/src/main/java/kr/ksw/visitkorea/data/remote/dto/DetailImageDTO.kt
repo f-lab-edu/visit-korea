a2e92dfd76081e516f5d1c2f21bb6a809b6946c4
ksw4015 <zealot4321@naver.com>
2024-10-10 11:05:24 +0900

    feat: add imgName params to DetailImageDTO class
    (cherry picked from commit 0483353287143c10c48f7afabe6f7a3c0cb6eefb)

--- app/src/main/java/kr/ksw/visitkorea/data/remote/dto/DetailImageDTO.kt
+++ app/src/main/java/kr/ksw/visitkorea/data/remote/dto/DetailImageDTO.kt
@@ -3,6 +3,8 @@ package kr.ksw.visitkorea.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class DetailImageDTO(
+    @SerializedName("imgname")
+    val imgName: String,
     @SerializedName("originimgurl")
     val originImgUrl: String,
     @SerializedName("smallimageurl")
