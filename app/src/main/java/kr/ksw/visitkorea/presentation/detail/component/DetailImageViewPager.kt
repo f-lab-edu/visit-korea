5c4156ee6b9614cb67182d7d5dc8fbb27768c716
ksw <zealot4321@dvercorp.co.kr>
2024-10-22 17:58:55 +0900

    design: change view pager ui design

--- app/src/main/java/kr/ksw/visitkorea/presentation/detail/component/DetailImageViewPager.kt
+++ app/src/main/java/kr/ksw/visitkorea/presentation/detail/component/DetailImageViewPager.kt
@@ -3,11 +3,13 @@ package kr.ksw.visitkorea.presentation.detail.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Surface
@@ -20,11 +22,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Size
-import kr.ksw.visitkorea.data.remote.dto.DetailImageDTO
 import kr.ksw.visitkorea.presentation.ui.theme.VisitKoreaTheme
 
 @Composable
@@ -49,12 +51,16 @@ fun DetailImageViewPager(
             key = { index ->
                 images[index]
             },
-            state = pagerState
+            state = pagerState,
+            contentPadding = PaddingValues(
+                horizontal = 16.dp
+            )
         ) { page ->
             AsyncImage(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(0.7f)
+                    .aspectRatio(0.75f)
+                    .padding(horizontal = 8.dp)
                     .background(color = Color.LightGray),
                 model = ImageRequest
                     .Builder(LocalContext.current)
